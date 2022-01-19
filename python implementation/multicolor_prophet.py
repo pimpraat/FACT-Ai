fb1f4df81de798496fee1bed1ed5d14f6751fda9
pimpraat <contact@pimpraat.com>
2022-01-19 15:32:07 +0100

    Update without prelim findings

--- python implementation/multicolor_prophet.py
+++ python implementation/multicolor_prophet.py
@@ -18,8 +18,14 @@
 
 # %%
 import numpy as np
-import matplotlib as plt
+import matplotlib.pyplot as plt
+from scipy.stats import uniform
 from numpy.random import default_rng
+from collections import Counter
+import matplotlib.pyplot as plt
+from statistics import mean
+
+rng = default_rng()
 
 
 # %% [markdown]
@@ -29,123 +35,41 @@ from numpy.random import default_rng
 # ## The two algorithms presented in the paper
 
 # %%
-def Finv(prob):
-    lower, upper = 0,1
+def Finv(distribution, prob):
+    lower, upper = 0.0,1.0
     if distribution == "uniform":
-        return lower + (upper - lower) * prob
-    else:
+        return prob * 1.0
+#         return lower + (upper - lower) * prob
+    if distribution == "binomial":
         return scipy.stats.binom.ppf(prob, n=1000, p=0.5)
         
 
-def FairGeneralProphet (F, q, V, distribution_type):
-    """
-    :param F: vector consisting of n distributions
-    :param q: vector containing probabilities q1, . . . , qn with which FAIROPT accepts candidate 1, ... , n
-    :param V: vector containing values for all candidates i, called from their F[i] distribution
-    :returns: 
-    """
-    s = 0
+def FairGeneralProphet (q, V, distribution_type):
+    summ = 0.0
     for i in range(0,len(V)):
-        if V[i] > Finv(Vlower, Vupper, (1- (q[i]/2)/(1-(s/2)))
+        if V[i] >= Finv(distribution_type, (1.0 - (q[i] / (2 - summ)))):
+#         if V[i] >= Finv(distribution_type, (1- (q[i]/2)/(1-(summ/2)))):
             return i
-        s += q[i]
+        summ += q[i]
 
-
-
-# %%
-def FairIIDProphet(F, Values):
+def FairIIDProphet(Values, distribution_type):
     """
     :param F:
     :returns: 
     """
     for i in range(0, len(Values)):
-        if Values[i] >= 1 - ((2/3*len(Values))/(1-2*(i-1)/3*len(Values))):
+        p = (2.0 / 3.0) / len(Values)
+        if Values[i] >= Finv(distribution_type, (1.0 - p / (1.0 - p * i))):
             return i
-
-
-# %% [markdown]
-# ## The mentioned baseline algorithms
-
-# %% [markdown]
-# * SC algorithm (Samuel-Cahn, 1984)
-#     * This algorithm sets a single threshold so that the maximum is above this threshold with probability exactly 1/2. 
-# * EHKS algorithm (Ehsani et al., 2018)
-#     * This algorithm sets a single threshold so that an individual candidate is accepted with probability 1/n
-# * CFHOV algorithm (Correa et al., 2021)
-#     * This algorithm sets a sequence of thresholds based on acceptance probabilities that result from solving a differential equation.
-# * DP algorithm (e.g. Chow et al., 1971)
-#     * This algorithm is the optimal threshold algorithm for the prophet problem, where thresholds are obtained by backward induction.
-
-# %%
-## TODO: Implement based on prepared pseudocode => ComputeSolutionOneHalf
-def SamualCahn(n_candidates):
-    """
-    :param F:
-    :returns: 
-    """
-    
-    for i in range (0,n):
-        
-        
-    
-    pass
+#         n = len(Values)
+#         deler = 2/(3*n)
+#         noemer = 1 - 2*(i - 2) / (3*n)
+#         if Values[i] >= Finv(distribution_type, (1 - deler/noemer)):
+#             return i
 
 
 # %%
-#TODO: Implement based on prepared pseudocode => ComputeSolutionOneMinusOneE
-def EHKS():
-    """
-    :param F:
-    :returns: 
-    """
-    pass
-
-
-# %%
-#TODO: Implement based on prepared pseudocode => ComputeSolutionThreeForth
-def CFHOV():
-    """
-    :param F:
-    :returns: 
-    """
-    pass
-
-
-# %%
-#TODO: Implement based on prepared pseudocode => ComputeSolutionDiffEq
-def DP_algorithm():
-    """
-    :param F:
-    :returns: 
-    """
-    pass
-
-
-# %% [markdown]
-# ## Some additional needed functions
-
-# %% [markdown]
-# We distinguish between the basic setting in which $p_j$ is the proportion of candidates that belong to group j, i.e., $p_j = n_j/n$, and the general setting in which p is arbitrary.
-
-# %%
-def generateProbabilityVector():
-    """
-    :returns p: probability vector with length j
-    """
-    return p
-
-
-# Don't to use this?? Ask TA
-def EFairOpt(n, C, F, p):
-    """
-    :param n: integer for the number of candidates
-    :param C: list of groups
-    :param F: vector of distribution (objects)
-    :param p: probability vector
-    :returns: 
-    """
-    pass
-
+# This cell will contain both the SC algorithm and the EHKS algorithms as supporting baselines
 
 # %% [markdown]
 # ## The experiments
@@ -157,33 +81,74 @@ def EFairOpt(n, C, F, p):
 # ### Uniform Distribution
 
 # %%
-rng = default_rng()
-import matplotlib.pyplot as plt
-
-# %%
 # input stream consists of 50 samples from the uniform distribution in range [0, 1]
 n = 50
-k = n # since each candidate is a group on its own
-C = range(0, n)
-n_vector = [1] * n
-q = [1/n] * n
-gen_input = rng.uniform(low=0.0, high=1.0, size=n)
+q, V = [1/n] * n , rng.uniform(low=0.0, high=1.0, size=n)
+
+N_experimentReps = 50000*2
+arrivalPositionsChosenFairPA, arrivalPositionsChosenFairIID = [0]*n, [0]*n
+a,b = [],[]
+# Running experiment for the FairPA
+for _ in range(N_experimentReps):
+    q, V = [1/n] * n , rng.uniform(low=0.0, high=1.0, size=n)
+    result = FairGeneralProphet(q, V, "uniform")
+    if result != None:
+        arrivalPositionsChosenFairPA[FairGeneralProphet(q, V, "uniform")] += 1
+        a.append(V[result])
+
+# Running experiment for the FairIID
+for _ in range(N_experimentReps):
+    q, V = [1/n] * n , rng.uniform(low=0.0, high=1.0, size=n)
+    if FairIIDProphet(V, "uniform") != None:
+        arrivalPositionsChosenFairIID[FairIIDProphet(V, "uniform")] += 1
+        b.append(V[result])
 
 
+            
 
-plt.scatter(gen_input)
+plt.plot(range(0,50), arrivalPositionsChosenFairPA, label="Fair PA")
+plt.plot(range(0,50), arrivalPositionsChosenFairIID, label="Fair IID")
+plt.plot(range(0,50), range(0,4000,80), label="replicate CFHOV for clearity")
+plt.title("100k experiments, discard None results")
+plt.xlabel("Arrival position")
+plt.ylabel("Num Picked")
+plt.legend(loc="upper right")
 
-# %% [markdown]
-# ### Biniomial Distribution
+print("The average value of the chosen candidate in the uniform distribution: \n)")
+# print("FairPA: ", mean(a), "(should be 0.501)")
+# print("FairIID: ", mean(b), "(should be 0.661)")
+
+
 
 # %%
 # input consists of 1000 samples from the binomial distribution with 1000 trials 
 # and 1/2 probability of success of a single trial
 n = 1000
 k = n # since each candidate is a group on its own
+q, V = [1/n] * n , rng.binomial(n=1000, p=.5, size=1000)
 
-gen_input = rng.binomial(n=1000, p=.5, size=1000)
-gen_input
-plt.plot(
+N_experimentReps = 500
+arrivalPositionsChosenFairPA, arrivalPositionsChosenFairIID = [0]*n, [0]*n
+
+# Running experiment for the FairPA
+for cnt in range(N_experimentReps):
+    print(cnt)
+    q, V = [1/n] * n , rng.binomial(n=1000, p=.5, size=1000)
+    result = FairGeneralProphet(q, V, "binomial")
+    if result != None:
+        arrivalPositionsChosenFairPA[FairGeneralProphet(q, V, "binomial")] += 1
+print("Ran FairPA")
+# Running experiment for the FairIID
+for cnt in range(N_experimentReps):
+    print("2: ", cnt)
+    q, V = [1/n] * n , rng.binomial(n=1000, p=.5, size=1000)
+    if FairIIDProphet(V, "binomial") != None:
+        arrivalPositionsChosenFairIID[FairIIDProphet(V, "binomial")] += 1
+            
+plt.plot(range(0,50), arrivalPositionsChosenFairPA, label="Fair PA")
+plt.plot(range(0,50), arrivalPositionsChosenFairIID, label="Fair IID")
+plt.xlabel("Arrival position")
+plt.ylabel("Num Picked")
+plt.legend(loc="upper right")
 
 # %%
