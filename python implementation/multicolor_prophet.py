73cf69c88add09424eb6ed07b810b69108378c45
pimpraat <contact@pimpraat.com>
2022-01-20 13:32:52 +0100

    Implemented baselines

--- python implementation/multicolor_prophet.py
+++ python implementation/multicolor_prophet.py
@@ -18,6 +18,7 @@
 
 # %%
 import numpy as np
+import scipy
 import matplotlib.pyplot as plt
 from scipy.stats import uniform
 from numpy.random import default_rng
@@ -29,7 +30,7 @@ rng = default_rng()
 
 
 # %% [markdown]
-# We next consider the following multi-color prophet prob- lem. In this model n candidates arrive in uniform random order. Candidates are partitioned into k groups $C = {C_1,···,C_k}$. We write $n=(n1,...,nk)$ for the vector of groupsizes, i.e., $|C_j| = n_j$ ,for all 1 ≤ j ≤ k. We identify each of the groups with a distinct color and let c(i), vi denote the color and value of candidate i, respectively. The value vi that is revealed upon arrival of i, and is drawn independently from a given distribution Fi. We use $F = (F_1, . . . , Fn)$ to refer to the vector of distributions. We are also given a probability vector $p = (p_1, . . . , p_k)$. The goal is to select a candidate in an online manner in order to maximize the expectation of the value of the selected candidate, while selecting from each color with probability proportional to p. We distinguish between the basic setting in which $p_j$ is the proportion of candidates that belong to group j, i.e., $p_j = n_j/n$, and the general setting in which $p$ is arbitrary. We compare ourselves with the fair optimum, the optimal offline algorithm that respects the $p_j$ ’s.
+# _We next consider the following multi-color prophet problem. In this model n candidates arrive in uniform random order. Candidates are partitioned into k groups $C = {C_1,···,C_k}$. We write $n=(n1,...,nk)$ for the vector of groupsizes, i.e., $|C_j| = n_j$ ,for all 1 ≤ j ≤ k. We identify each of the groups with a distinct color and let c(i), vi denote the color and value of candidate i, respectively. The value vi that is revealed upon arrival of i, and is drawn independently from a given distribution Fi. We use $F = (F_1, . . . , Fn)$ to refer to the vector of distributions. We are also given a probability vector $p = (p_1, . . . , p_k)$. The goal is to select a candidate in an online manner in order to maximize the expectation of the value of the selected candidate, while selecting from each color with probability proportional to p. We distinguish between the basic setting in which $p_j$ is the proportion of candidates that belong to group j, i.e., $p_j = n_j/n$, and the general setting in which $p$ is arbitrary. We compare ourselves with the fair optimum, the optimal offline algorithm that respects the $p_j$ ’s._
 
 # %% [markdown]
 # ## The two algorithms presented in the paper
@@ -38,12 +39,21 @@ rng = default_rng()
 def Finv(distribution, prob):
     lower, upper = 0.0,1.0
     if distribution == "uniform":
-        return prob * 1.0
-#         return lower + (upper - lower) * prob
+        return prob * (upper-lower)
     if distribution == "binomial":
         return scipy.stats.binom.ppf(prob, n=1000, p=0.5)
         
 
+## Is different for the Binomial!
+def MiddleUniform(n):
+    rrange = 1.0
+    return rrange * np.power(1.0 / 2, 1.0 / n)
+    
+def MiddleBinomialDistribution(n):
+    
+
+#TODO: make class for the distributions!!
+        
 def FairGeneralProphet (q, V, distribution_type):
     summ = 0.0
     for i in range(0,len(V)):
@@ -68,6 +78,22 @@ def FairIIDProphet(Values, distribution_type):
 #             return i
 
 
+# SC: second method “ComputeSolutionOneHalf”
+def SC_algorithm(Values, distribution_type):
+    for i in range(0, len(Values)):
+        # distributions[elements[i].type].get().Middle(elements.size())
+        if Values[i] >= MiddleUniform(len(Values)):
+            return i
+
+
+# EHKS: first method “ComputeSolutionOneMinusOneE”
+def EHKS_algorithm(Values, distribution_type):
+    for i in range(0, len(Values)):
+        if Values[i] >= Finv(distribution_type, (1.0 - (1.0 / len(Values)))):
+            return i
+
+
+
 # %%
 # This cell will contain both the SC algorithm and the EHKS algorithms as supporting baselines
 
@@ -75,7 +101,7 @@ def FairIIDProphet(Values, distribution_type):
 # ## The experiments
 
 # %% [markdown]
-# *"We focus on the case, where values are distributed i.i.d. and each candidate is a group on its own. We consider two settings. In the first one the input stream consists of 50 samples from the uniform distribution in range [0, 1], and in the second one the input consists of 1000 samples from the binomial distribution with 1000 trials and 1/2 probability of success of a single trial. For better comparability with existing algorithms, in both cases we assume each candidate is a group on its own. We run each algorithm 50, 000 times."*
+# _"We focus on the case, where values are distributed i.i.d. and each candidate is a group on its own. We consider two settings. In the first one the input stream consists of 50 samples from the uniform distribution in range [0, 1], and in the second one the input consists of 1000 samples from the binomial distribution with 1000 trials and 1/2 probability of success of a single trial. For better comparability with existing algorithms, in both cases we assume each candidate is a group on its own. We run each algorithm 50, 000 times."_
 
 # %% [markdown]
 # ### Uniform Distribution
@@ -85,11 +111,17 @@ def FairIIDProphet(Values, distribution_type):
 n = 50
 q, V = [1/n] * n , rng.uniform(low=0.0, high=1.0, size=n)
 
+print(SC_algorithm(V, "uniform"))
+print(EHKS_algorithm(V, "uniform"))
+
 N_experimentReps = 50000*2
 arrivalPositionsChosenFairPA, arrivalPositionsChosenFairIID = [0]*n, [0]*n
-a,b = [],[]
+arrivalPositionsChosenSC, arrivalPositionsChosenEHKS = [0]*n, [0]*n
+a,b,c,d = [],[],[],[]
 # Running experiment for the FairPA
 for _ in range(N_experimentReps):
+    result = None
+#     while (result == None):
     q, V = [1/n] * n , rng.uniform(low=0.0, high=1.0, size=n)
     result = FairGeneralProphet(q, V, "uniform")
     if result != None:
@@ -98,55 +130,89 @@ for _ in range(N_experimentReps):
 
 # Running experiment for the FairIID
 for _ in range(N_experimentReps):
+    result = None
+#     while (result == None):
     q, V = [1/n] * n , rng.uniform(low=0.0, high=1.0, size=n)
-    if FairIIDProphet(V, "uniform") != None:
+    result = FairIIDProphet(V, "uniform")
+    if result != None:
         arrivalPositionsChosenFairIID[FairIIDProphet(V, "uniform")] += 1
-        b.append(V[result])
-
-
+        b.append(V[FairIIDProphet(V, "uniform")])
+        
+# Running experiment for the SC
+for _ in range(N_experimentReps):
+    result = None
+#     while (result == None):
+    q, V = [1/n] * n , rng.uniform(low=0.0, high=1.0, size=n)
+    result = SC_algorithm(V, "uniform")
+    if result != None:
+        arrivalPositionsChosenSC[SC_algorithm(V, "uniform")] += 1
+        c.append(V[SC_algorithm(V, "uniform")])
+        
+# Running experiment for the EHKS
+for _ in range(N_experimentReps):
+    result = None
+#     while (result == None):
+    q, V = [1/n] * n , rng.uniform(low=0.0, high=1.0, size=n)
+    result = EHKS_algorithm(V, "uniform")
+    if result != None:
+        arrivalPositionsChosenEHKS[EHKS_algorithm(V, "uniform")] += 1
+        d.append(V[EHKS_algorithm(V, "uniform")])
             
 
 plt.plot(range(0,50), arrivalPositionsChosenFairPA, label="Fair PA")
 plt.plot(range(0,50), arrivalPositionsChosenFairIID, label="Fair IID")
-plt.plot(range(0,50), range(0,4000,80), label="replicate CFHOV for clearity")
-plt.title("100k experiments, discard None results")
+plt.plot(range(0,50), arrivalPositionsChosenEHKS, label="EHKS")
+plt.plot(range(0,50), arrivalPositionsChosenSC, label="SC")
+plt.plot(range(0,50), range(0,4000,80), label="replicate CFHOV for scale")
+plt.title("100k experiments, discarding None results")
 plt.xlabel("Arrival position")
 plt.ylabel("Num Picked")
 plt.legend(loc="upper right")
 
 print("The average value of the chosen candidate in the uniform distribution: \n)")
-# print("FairPA: ", mean(a), "(should be 0.501)")
-# print("FairIID: ", mean(b), "(should be 0.661)")
+print("FairPA: ", mean(a), "(should be 0.501)")
+print("FairIID: ", mean(b), "(should be 0.661)")
 
 
+
+# %% [markdown]
+# This led us to examining two approaches.
+#
+# It is also worth noting that just skipping over None results does not lead to the same results, only increasing the number of experiments. As shown here
+
+# %% [markdown]
+# ## Binomial distribution
 
 # %%
 # input consists of 1000 samples from the binomial distribution with 1000 trials 
 # and 1/2 probability of success of a single trial
-n = 1000
+n = 200
 k = n # since each candidate is a group on its own
-q, V = [1/n] * n , rng.binomial(n=1000, p=.5, size=1000)
+q, V = [1/n] * n , rng.binomial(n=1000, p=.5, size=n)
 
-N_experimentReps = 500
+N_experimentReps = 2000
 arrivalPositionsChosenFairPA, arrivalPositionsChosenFairIID = [0]*n, [0]*n
 
 # Running experiment for the FairPA
 for cnt in range(N_experimentReps):
-    print(cnt)
-    q, V = [1/n] * n , rng.binomial(n=1000, p=.5, size=1000)
+#     print(cnt)
+    if (cnt % 50 == 0) : print(cnt)
+    q, V = [1/n] * n , rng.binomial(n=1000, p=.5, size=n)
     result = FairGeneralProphet(q, V, "binomial")
     if result != None:
         arrivalPositionsChosenFairPA[FairGeneralProphet(q, V, "binomial")] += 1
-print("Ran FairPA")
+# print("Ran FairPA")
+
 # Running experiment for the FairIID
 for cnt in range(N_experimentReps):
-    print("2: ", cnt)
-    q, V = [1/n] * n , rng.binomial(n=1000, p=.5, size=1000)
+#     print("2: ", cnt)
+    if cnt % 50 : print(cnt)
+    q, V = [1/n] * n , rng.binomial(n=1000, p=.5, size=n)
     if FairIIDProphet(V, "binomial") != None:
         arrivalPositionsChosenFairIID[FairIIDProphet(V, "binomial")] += 1
             
-plt.plot(range(0,50), arrivalPositionsChosenFairPA, label="Fair PA")
-plt.plot(range(0,50), arrivalPositionsChosenFairIID, label="Fair IID")
+plt.plot(range(0,n), arrivalPositionsChosenFairPA, label="Fair PA")
+plt.plot(range(0,n), arrivalPositionsChosenFairIID, label="Fair IID")
 plt.xlabel("Arrival position")
 plt.ylabel("Num Picked")
 plt.legend(loc="upper right")
