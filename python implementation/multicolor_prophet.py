87c20699b7472aba510dd22f0fb3fab3242bbb47
pimpraat <contact@pimpraat.com>
2022-01-20 16:26:59 +0100

    Cleaned up code

--- python implementation/multicolor_prophet.py
+++ python implementation/multicolor_prophet.py
@@ -16,6 +16,9 @@
 # %% [markdown]
 # # The Multi-Color Prophet Problem notebook
 
+# %% [markdown]
+#  Contact: p.praat@student.uva.nl
+
 # %%
 import numpy as np
 import scipy
@@ -50,7 +53,7 @@ def MiddleUniform(n):
     return rrange * np.power(1.0 / 2, 1.0 / n)
     
 def MiddleBinomialDistribution(n):
-    
+    pass
 
 #TODO: make class for the distributions!!
         
@@ -63,39 +66,28 @@ def FairGeneralProphet (q, V, distribution_type):
         summ += q[i]
 
 def FairIIDProphet(Values, distribution_type):
-    """
-    :param F:
-    :returns: 
-    """
     for i in range(0, len(Values)):
         p = (2.0 / 3.0) / len(Values)
         if Values[i] >= Finv(distribution_type, (1.0 - p / (1.0 - p * i))):
-            return i
-#         n = len(Values)
-#         deler = 2/(3*n)
-#         noemer = 1 - 2*(i - 2) / (3*n)
-#         if Values[i] >= Finv(distribution_type, (1 - deler/noemer)):
-#             return i
-
-
-# SC: second method “ComputeSolutionOneHalf”
-def SC_algorithm(Values, distribution_type):
-    for i in range(0, len(Values)):
-        # distributions[elements[i].type].get().Middle(elements.size())
-        if Values[i] >= MiddleUniform(len(Values)):
-            return i
-
-
-# EHKS: first method “ComputeSolutionOneMinusOneE”
-def EHKS_algorithm(Values, distribution_type):
-    for i in range(0, len(Values)):
-        if Values[i] >= Finv(distribution_type, (1.0 - (1.0 / len(Values)))):
             return i
 
 
 
 # %%
-# This cell will contain both the SC algorithm and the EHKS algorithms as supporting baselines
+# Implemented according to the function “ComputeSolutionOneHalf” in unfair-prophet.cc
+#TODO: Right now only works for uniform distribution!
+def SC_algorithm(Values, distribution_type):
+    for i in range(0, len(Values)):
+        if Values[i] >= MiddleUniform(len(Values)):
+            return i
+
+
+# Implemented according to the function “ComputeSolutionOneMinusOneE” in unfair-prophet.cc
+def EHKS_algorithm(Values, distribution_type):
+    for i in range(0, len(Values)):
+        if Values[i] >= Finv(distribution_type, (1.0 - (1.0 / len(Values)))):
+            return i
+
 
 # %% [markdown]
 # ## The experiments
@@ -106,58 +98,91 @@ def EHKS_algorithm(Values, distribution_type):
 # %% [markdown]
 # ### Uniform Distribution
 
+# %% [markdown]
+# _Here, as mentioned in the paper, the "input stream consists of 50 samples from the uniform distribution in range [0, 1]"_
+
 # %%
-# input stream consists of 50 samples from the uniform distribution in range [0, 1]
-n = 50
-q, V = [1/n] * n , rng.uniform(low=0.0, high=1.0, size=n)
+def generateDistribution(distribution_type, size):
+    if distribution_type == "uniform":
+        q, V = [1/n] * n , rng.uniform(low=0.0, high=1.0, size=n)
+    elif distribution_type == "binomial":
+        q, V = [1/n] * n , rng.binomial(n=1000, p=.5, size=n)
+    return q,V
 
-print(SC_algorithm(V, "uniform"))
-print(EHKS_algorithm(V, "uniform"))
-
-N_experimentReps = 50000*2
-arrivalPositionsChosenFairPA, arrivalPositionsChosenFairIID = [0]*n, [0]*n
-arrivalPositionsChosenSC, arrivalPositionsChosenEHKS = [0]*n, [0]*n
-a,b,c,d = [],[],[],[]
-# Running experiment for the FairPA
-for _ in range(N_experimentReps):
-    result = None
-#     while (result == None):
-    q, V = [1/n] * n , rng.uniform(low=0.0, high=1.0, size=n)
-    result = FairGeneralProphet(q, V, "uniform")
-    if result != None:
-        arrivalPositionsChosenFairPA[FairGeneralProphet(q, V, "uniform")] += 1
-        a.append(V[result])
-
-# Running experiment for the FairIID
-for _ in range(N_experimentReps):
-    result = None
-#     while (result == None):
-    q, V = [1/n] * n , rng.uniform(low=0.0, high=1.0, size=n)
-    result = FairIIDProphet(V, "uniform")
-    if result != None:
-        arrivalPositionsChosenFairIID[FairIIDProphet(V, "uniform")] += 1
-        b.append(V[FairIIDProphet(V, "uniform")])
+"""
+:param algorithm:
+:param N_experimentReps: 
+:param Values: values
+:param distribution_type:
+:param q: 
+:returns: arrivalPositionsChosen, chosenValues
+"""
+def runExperiment(algorithm, N_experimentReps, distribution_type, n_candidates):
+    arrivalPositionsChosen, chosenValues = [0]*n_candidates, []
+    for _ in range(0, N_experimentReps):
+        q, Values = generateDistribution(distribution_type, n_candidates)
         
-# Running experiment for the SC
-for _ in range(N_experimentReps):
-    result = None
-#     while (result == None):
-    q, V = [1/n] * n , rng.uniform(low=0.0, high=1.0, size=n)
-    result = SC_algorithm(V, "uniform")
-    if result != None:
-        arrivalPositionsChosenSC[SC_algorithm(V, "uniform")] += 1
-        c.append(V[SC_algorithm(V, "uniform")])
-        
-# Running experiment for the EHKS
-for _ in range(N_experimentReps):
-    result = None
-#     while (result == None):
-    q, V = [1/n] * n , rng.uniform(low=0.0, high=1.0, size=n)
-    result = EHKS_algorithm(V, "uniform")
-    if result != None:
-        arrivalPositionsChosenEHKS[EHKS_algorithm(V, "uniform")] += 1
-        d.append(V[EHKS_algorithm(V, "uniform")])
+        if algorithm == "FairGeneralProphet":
+                result = FairGeneralProphet(q, Values, distribution_type)
+        elif algorithm == "FairIIDProphet":
+                result = FairIIDProphet(Values, distribution_type)
+        elif algorithm == "SC":
+                result = SC_algorithm(Values, distribution_type)
+        elif algorithm =="EHKS":
+                result = EHKS_algorithm(Values, distribution_type)
+                
+        if result != None:
+            arrivalPositionsChosen[result] += 1
+            chosenValues.append(Values[result])
             
+        if result == None: chosenValues.append(0)
+    return arrivalPositionsChosen, chosenValues
+
+
+# %%
+#Plotting the results for 50k experiments
+
+arrivalPositionsChosenFairPA, a = runExperiment(algorithm="FairGeneralProphet", N_experimentReps=50000, 
+                                                distribution_type="uniform", n_candidates=50)
+    
+arrivalPositionsChosenFairIID, b = runExperiment(algorithm="FairIIDProphet", N_experimentReps=50000, 
+                                                distribution_type="uniform", n_candidates=50)
+    
+arrivalPositionsChosenSC, c = runExperiment(algorithm="SC", N_experimentReps=50000, 
+                                                distribution_type="uniform", n_candidates=50)
+    
+arrivalPositionsChosenEHKS, d = runExperiment(algorithm="EHKS", N_experimentReps=50000, 
+                                                distribution_type="uniform", n_candidates=50)
+
+plt.plot(range(0,50), arrivalPositionsChosenFairPA, label="Fair PA")
+plt.plot(range(0,50), arrivalPositionsChosenFairIID, label="Fair IID")
+plt.plot(range(0,50), arrivalPositionsChosenEHKS, label="EHKS")
+plt.plot(range(0,50), arrivalPositionsChosenSC, label="SC")
+plt.plot(range(0,50), range(0,4000,80), label="replicate CFHOV for scale")
+plt.title("50k experiments, discarding None results")
+plt.xlabel("Arrival position")
+plt.ylabel("Num Picked")
+plt.legend(loc="upper right")
+
+# %% [markdown]
+# This led us to examining two approaches. One in which we increase the number of experiments to 100k, and one where we run 50k experiments and keep repeating each experiment untill we don't get a None. The first one has been done above, and seems to confirm the found results.
+#
+# It is also worth noting that just skipping over None results does not lead to the same results, only increasing the number of experiments. As shown here
+
+# %%
+#Plotting the results for 100k experiments
+
+arrivalPositionsChosenFairPA, a = runExperiment(algorithm="FairGeneralProphet", N_experimentReps=50000, 
+                                                distribution_type="uniform", n_candidates=50)
+    
+arrivalPositionsChosenFairIID, b = runExperiment(algorithm="FairIIDProphet", N_experimentReps=50000, 
+                                                distribution_type="uniform", n_candidates=50)
+    
+arrivalPositionsChosenSC, c = runExperiment(algorithm="SC", N_experimentReps=50000, 
+                                                distribution_type="uniform", n_candidates=50)
+    
+arrivalPositionsChosenEHKS, d = runExperiment(algorithm="EHKS", N_experimentReps=50000, 
+                                                distribution_type="uniform", n_candidates=50)
 
 plt.plot(range(0,50), arrivalPositionsChosenFairPA, label="Fair PA")
 plt.plot(range(0,50), arrivalPositionsChosenFairIID, label="Fair IID")
@@ -169,16 +194,46 @@ plt.xlabel("Arrival position")
 plt.ylabel("Num Picked")
 plt.legend(loc="upper right")
 
-print("The average value of the chosen candidate in the uniform distribution: \n)")
+# %% [markdown]
+# ### Average values of chosen candidates
+
+# %%
+print("The average value of the chosen candidate in the uniform distribution: \n")
 print("FairPA: ", mean(a), "(should be 0.501)")
 print("FairIID: ", mean(b), "(should be 0.661)")
+print("SK: ", mean(c), "(should be 0.499)")
+print("EHKS: ", mean(d), "(should be 0.631)")
+
+# %% [markdown]
+#
+# **Statement 1:** _In conclusion, for both settings, both our algorithms Algorithm 2 and Algorithm 3 provide perfect fairness, while giving 66.71% and 88.01% (for the uniform case), and 58.12% and 75.82% (for the binomial case), of the value of the optimal, but unfair, online algorithm._
+#
+# Here the unfair online algorihtm must either SC or EHKS, something else is not mentioned about online algorithms. Also in the codebase, the unfair algorithms refer to these. Thus we will take a look at both, and see if these results are replicable.
+#
+
+# %%
+# TotalValueFairPA = [x for x in a if x != 0]
+# TotalValueFairIID = [x for x in b if x != 0]
+# TotalValueFairSK = [x for x in c if x != 0]
+# TotalValueFairEHKS = [x for x in d if x != 0]
+
+# a = TotalValueFairPA
+# b = TotalValueFairIID
+# c = TotalValueFairSK 
+# d = TotalValueFairEHKS 
+
+print("Uniform case, for FairPA")
+print("Assuming SK as the 'optimal, but unfair, online algorithm' :", mean(a) / mean(c) *100, "%")
+print("Assuming EHKS as the 'optimal, but unfair, online algorithm' :", mean(a) / mean(d) *100, "%")
+
+print("\n Uniform case, for FairIID")
+print("Assuming SK as the 'optimal, but unfair, online algorithm' :", mean(b) / mean(c) *100, "%")
+print("Assuming EHKS as the 'optimal, but unfair, online algorithm' :", mean(b) / mean(d) *100, "%")
 
 
 
 # %% [markdown]
-# This led us to examining two approaches.
-#
-# It is also worth noting that just skipping over None results does not lead to the same results, only increasing the number of experiments. As shown here
+# Both of these approaches thus do not (nearly) approach the results as presented in the paper, and seem to be verifiable in Figure 2 in the paper. Thus, the question is raised which algorithm is used to calculate the percentages mentioned in **Statement 1**.
 
 # %% [markdown]
 # ## Binomial distribution
